afd02f4e226c44a57e4059f86ed7454ff6362d0e
JKTM1KMNTIKDL15\arntonius <marcellantonius18@gmail.com>
2022-06-25 07:21:09 +0700

    create title base on component collection

--- src/pages/Collection/index.jsx
+++ src/pages/Collection/index.jsx
@@ -15,6 +15,9 @@ const Collection = () => {
     const main = css({
         display: 'flex',
         flexDirection: 'column',
+        'h1,h3,p': {
+            margin: 0
+        }
     })
 
     const container = css({
@@ -36,11 +39,10 @@ const Collection = () => {
 
     const imageBanner = css({
         width: '100%',
-        height: 600,
+        height: 530,
         objectFit: 'cover',
         [mq[0]]: {
             height: 400,
-            width: 'auto',
             borderRadius: 20,
         }
     })
@@ -99,15 +101,85 @@ const Collection = () => {
     const baseLayerImage = css({
         backgroundColor: 'black',
         opacity: 0.3,
-        borderRadius: 20,
         position: 'absolute',
         zIndex: 2,
         width: '100%',
-        height: '100%'
+        height: '100%',
+        [mq[0]]: {
+            borderRadius: 20,
+        }
     })
 
     const titleText = css({
         color: 'white'
+    })
+
+
+    const bundleCollection = css({
+        margin: '0px 16px'
+    })
+
+    const headerList = css({
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'space-between'
+    })
+
+    const buttonExplore = css({
+        color: '#3B82F6',
+        textDecoration: 'none',
+        marginLeft: 20,
+        display: 'none',
+        [mq[0]]: {
+            display: 'block'
+        }
+    })
+
+    const mainTitle = css({
+        display: 'flex',
+        alignItems: 'center'
+    })
+
+    const textTitle = css({
+        fontSize: 20,
+        [mq[0]]: {
+            fontSize: 24
+        }
+    })
+
+    const mainAction = css({
+        display: 'flex',
+        alignItems: 'center'
+    })
+
+    const iconPen = css({
+        fontSize: 12,
+        backgroundColor: '#9CA3AF',
+        color: 'white',
+        padding: 6,
+        borderRadius: 20,
+        ':hover': {
+            backgroundColor: '#FACC15',
+        },
+        [mq[0]]: {
+            padding: 8,
+            fontSize: 16,
+        }
+    })
+
+    const iconTrash = css({
+        fontSize: 12,
+        backgroundColor: '#9CA3AF',
+        color: 'white',
+        padding: 6,
+        borderRadius: 20,
+        ':hover': {
+            backgroundColor: '#F87181',
+        },
+        [mq[0]]: {
+            padding: 8,
+            fontSize: 16,
+        }
     })
     return (
         <div css={main}>
@@ -115,7 +187,7 @@ const Collection = () => {
             <div css={container}>
                 <div css={banner}>
                     <div css={bundleImage}>
-                        <div css={baseLayerImage}> <h1>asda</h1></div>
+                        <div css={baseLayerImage}></div>
                         <img css={imageBanner} src={'https://s4.anilist.co/file/anilistcdn/media/anime/banner/1-T3PJUjFJyRwg.jpg'} alt="banner-image-anime" />
                     </div>
                     <div css={infoBanner}>
@@ -138,6 +210,23 @@ const Collection = () => {
                             </div>
                         </div>
                         <p css={descriptionText}>Enter a world in the distant future, where Bounty Hunters roam the solar system. Spike and Jet, bounty hunting partners, set out on journeys in an ever struggling effort to win bounty rewards to survive.While traveling, they meet up with other very interesting people. Could Faye, the beautiful and ridiculously poor gambler, Edward, the computer genius, and Ein, the engineered dog be a good addition to the group?</p>
+                    </div>
+                </div>
+                <Gap height={50}></Gap>
+                <div css={bundleCollection}>
+                    <div css={headerList}>
+                        <div css={mainTitle}>
+                            <h1 css={textTitle}>My Popular Collections</h1>
+                            <NavLink to="/collection-detail/1" css={buttonExplore}>
+                                <h3 >Explore All</h3>
+                            </NavLink>
+                        </div>
+                        <div css={mainAction}>
+                            <FontAwesomeIcon icon="pen" css={iconPen} />
+                            <Gap width={10}></Gap>
+                            <FontAwesomeIcon icon="trash" css={iconTrash} />
+                        </div>
+
                     </div>
                 </div>
             </div>
--- src/utils/fontAwesome/index.jsx
+++ src/utils/fontAwesome/index.jsx
@@ -1,4 +1,26 @@
 import { library } from '@fortawesome/fontawesome-svg-core'
-import { faCheckSquare, faCoffee, faFilm, faClapperboard, faClock, faCalendar, faFlag, faHeart } from '@fortawesome/free-solid-svg-icons'
+import {
+    faCheckSquare,
+    faCoffee,
+    faFilm,
+    faClapperboard,
+    faClock,
+    faCalendar,
+    faFlag,
+    faHeart,
+    faTrash,
+    faPen
+} from '@fortawesome/free-solid-svg-icons'
 
-library.add(faCheckSquare, faCoffee, faFilm, faClapperboard, faClock, faCalendar, faFlag, faHeart)
+library.add(
+    faCheckSquare,
+    faCoffee,
+    faFilm,
+    faClapperboard,
+    faClock,
+    faCalendar,
+    faFlag,
+    faHeart,
+    faTrash,
+    faPen
+)
